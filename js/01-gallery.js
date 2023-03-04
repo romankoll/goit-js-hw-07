138a8f1fe53b9ccd6e72fc9570ac78a0e99f4a96
Roman Kolobych <romankol1223@gmail.com>
2023-03-04 21:34:21 +0200

    1 задача не виходить

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallerySectionEll = document.querySelectorAll(".gallery");
+gallerySectionEll.addEventListener("click", handleImgClick);
+
+function createGallary(obj) {
+  return obj.map(
+    (ell) => `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${ell.original}">
+        <img
+          class="gallery__image"
+          src="${ell.preview}"
+          data-source="${ell.original}"
+          alt="${ell.description}"
+        />
+      </a>
+    </div>`
+  );
+}
+
+const addGalleryItemsList = createGallary(galleryItems).slice(",").join(" ");
+
+gallerySectionEll.insertAdjacentHTML("beforeend", addGalleryItemsList);
+
+function handleImgClick(event) {
+  event.preventDefault();
+  const instance = basicLightbox
+    .create(`<img src="${event.target.dataset.source}" width="1280">`)
+    .show();
+
+  if (event.target.nodeName === "IMG") {
+    return instance;
+  } else {
+    return;
+  }
+}
